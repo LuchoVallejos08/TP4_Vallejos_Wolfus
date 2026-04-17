8fd3b648aed1fe55bd92d4c2bea2f20a77234231
MATÍAS WOLFUS <49194355@ort.edu.ar>
2026-04-17 11:57:08 -0300

    -

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { mostrarHora } from "./time.js";
+import { mostrarFecha, mostrarHora } from "./time.js";
 
 const app = express();
 const port = 3000;
@@ -13,6 +13,11 @@ app.get('/hora', (req, res) => {
     const horaActual = mostrarHora();
     res.send(`La hora actual es: ${horaActual}`);
 })
+app.get('/fechaCompleta', (req, res) => {
+    const fechaActual = mostrarFecha();
+    res.send(`La hora actual es: ${fechaActual}`);
+})
+
 app.listen(port, () => {
     console.log(`Listening to http://localhost:${port}`);
 })
@@ -20,3 +25,4 @@ app.listen(port, () => {
 app.get("/error", (req, res) => {
     res.status(404).send("Página no encontrada");
 });
+
--- time.js
+++ time.js
@@ -1,7 +1,16 @@
 import dayjs from "dayjs";
+import 'dayjs/locale/es.js';
+dayjs.locale('es');
 
 function mostrarHora() {
     const horaActual = dayjs().format("HH:mm:ss");
     return horaActual;
 }
-export { mostrarHora };
+
+function mostrarFecha() {
+    const fechaCompleta = dayjs().format("dddd, D [de] MMMM [de] YYYY HH:mm:ss");
+    return fechaCompleta;
+}
+
+export { mostrarHora, mostrarFecha };
+
